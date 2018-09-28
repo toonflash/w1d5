7dd8b98b4373fdfa402063fddc3ff73c07085bf2
toonflash <toonflash@hotmail.com>
2018-09-28 18:47:04 +0000

    initializing week 1 day 5 repo

---
+++ npm-install-chalk/index.js
@@ -0,0 +1,9 @@
+var chalk = require("chalk");
+
+var message = "Hello " + chalk.yellow("World");
+var message2 = "Hello " + chalk.green("World");
+
+
+
+console.log(message);
+console.log(message2);
